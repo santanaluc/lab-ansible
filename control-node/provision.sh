dc7ed63242638c9705b726f78738a03e5613a71e
Lucas Santana <lucas.s.s.silva@hotmail.com>
2022-04-29 15:37:17 -0300

    devops IaC ansible: configuracao inicial do sistema para aplicação java

---
+++ control-node/provision.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Instalar o ansible
+sudo yum -y install epel-release
+echo "Início da instalação do ansible"
+sudo yum -y install ansible
+
+cat <<EOT >> /etc/hosts
+192.168.2.2 control-node
+192.168.2.3 app01
+192.168.2.4 db01
+EOT 
---
+++ db01/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.21.0"]],["vagrant-winnfsd",["= 1.4.0"]]],"checksum":"636bb69badcb610362cbe6baefd335264688c04649a9a1fd01c72263a6df7adb","vagrant_version":"2.2.19"}
