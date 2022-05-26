5141b2b5cb23094c107243b239150e40a1db6c79
Lucas Santana <lucas.s.s.silva@hotmail.com>
2022-05-25 23:57:15 -0300

    finalizando

--- control-node/.vagrant/bundler/global.sol
+++ control-node/.vagrant/bundler/global.sol
@@ -1,1 +1,1 @@
-{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.21.0"]],["vagrant-winnfsd",["= 1.4.0"]]],"checksum":"636bb69badcb610362cbe6baefd335264688c04649a9a1fd01c72263a6df7adb","vagrant_version":"2.2.19"}
+{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.30.0"]],["vagrant-winnfsd",["= 1.4.0"]]],"checksum":"d18624649c059b2429e9c51cf807863067167ef1e192f5b7a3822687ccb4a2ae","vagrant_version":"2.2.19"}
